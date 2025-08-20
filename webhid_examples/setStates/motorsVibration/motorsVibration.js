959229f3364b8fc94b41999a913cf53fb08a3246
amerca9664 <amerca9664@gmail.com>
2025-08-19 23:29:56 -0500

    fix: 2

--- webhid_examples/setStates/motorsVibration/motorsVibration.js
+++ webhid_examples/setStates/motorsVibration/motorsVibration.js
@@ -7,7 +7,7 @@ const closeElement = document.getElementById('closeButton');
 const ds = new jsDualsense();
 
 startElement.addEventListener('click', async () => {
-	await ds.start(); //Connect
+	await ds.start(); // Connect
 
 	await ds.setVibrationL.setVibration(255);
 	await ds.setVibrationR.setVibration(255);
