95186376eceb4b4391ad118307b1fa67861fcbaf
amerca9664 <amerca9664@gmail.com>
2025-08-20 14:24:38 -0500

    feat: add readState example (#24)
    * feat: add readState example
    
    * fix: comment about imports readState

---
+++ webhid_examples/readStates/readState.js
@@ -0,0 +1,49 @@
+// Local import for development or internal use
+import { jsDualsense } from 'jsdualsense';
+
+// Import from CDN for environments where packages can't be installed (e.g., directly in browsers)
+//import {jsDualsense, TrigerEffects,} from 'https://cdn.skypack.dev/jsdualsense';
+
+const connectElement = document.getElementById('connect');
+const readElement = document.getElementById('read');
+const closeElement = document.getElementById('close');
+const showTextElement = document.getElementById('showText');
+
+const ds = new jsDualsense();
+
+connectElement.addEventListener('click', async () => {
+	await ds.start();
+});
+
+let isReading = false;
+readElement.addEventListener('click', () => {
+	const readLoop = async () => {
+		if (isReading) {
+			isReading = false;
+			return;
+		}
+
+		isReading = true;
+
+		try {
+			const data = await ds.readData();
+			showTextElement.textContent = JSON.stringify(data);
+
+			isReading = false;
+		} catch (error) {
+			console.error(error);
+			isReading = false;
+			return;
+		}
+
+		setTimeout(readLoop, 10); // Wait until the next read
+	};
+
+	readLoop(); // Start cicle
+});
+
+closeElement.addEventListener('click', async () => {
+	isReading = true;
+
+	await ds.finish();
+});
