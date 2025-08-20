2fccf09cb745fbf2efb07cfe2da1f2281a27bf4a
amerca9664 <amerca9664@gmail.com>
2025-08-19 21:24:15 -0500

    feat: add audio example

---
+++ webhid_examples/setStates/audioFeatures/audioFeatures.js
@@ -0,0 +1,23 @@
+import { jsDualsense, MicSelect, AudioSpkSelect } from 'jsdualsense';
+// import { jsDualsense, MicSelect, AudioSpkSelect } from 'https://cdn.skypack.dev/jsdualsense';
+
+const startElement = document.getElementById('connectButton');
+const closeElement = document.getElementById('closeButton');
+
+const ds = new jsDualsense();
+
+startElement.addEventListener('click', async () => {
+	await ds.start(); // Connect
+
+	// Activar bocina interna y seleccionar micrófono externo
+	await ds.setAudio.setAudioOutput(AudioSpkSelect.Speaker);
+	await ds.setAudio.setMicOutput(MicSelect.External);
+
+	// Encender LED del micrófono y silenciarlo
+	await ds.setAudio.setMicrophoneLED(true);
+	await ds.setAudio.setMicrophoneState(true);
+});
+
+closeElement.addEventListener('click', async () => {
+	await ds.finish(); // Disconnect
+});
